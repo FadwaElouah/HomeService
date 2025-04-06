a62f30e241754503b79f009385b336a3d2dccd50
FadwaElouah <fadwaelouah@gmail.com>
2025-04-07 00:30:54 +0100

    did user

--- Home-servcie/database/migrations/2014_10_12_000000_create_users_table.php
+++ Home-servcie/database/migrations/2014_10_12_000000_create_users_table.php
@@ -9,14 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'client', 'provider']);
+            $table->string('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->string('profile_image')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
