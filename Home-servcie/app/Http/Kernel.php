b4e9b2d110e85f09698c72dd9faf146658e2954a
FadwaElouah <fadwaelouah@gmail.com>
2025-04-07 00:56:04 +0100

    did middleware

--- Home-servcie/app/Http/Kernel.php
+++ Home-servcie/app/Http/Kernel.php
@@ -64,5 +64,8 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'admin' => \App\Http\Middleware\CheckRole::class.':admin',
+        'client' => \App\Http\Middleware\CheckRole::class.':client',
+        'provider' => \App\Http\Middleware\CheckRole::class.':provider',
     ];
 }
