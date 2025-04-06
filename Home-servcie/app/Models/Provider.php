e70699993108fe279940dc153d6bc55b00e64501
FadwaElouah <fadwaelouah@gmail.com>
2025-04-07 00:37:10 +0100

    did fillable Provider

--- Home-servcie/app/Models/Provider.php
+++ Home-servcie/app/Models/Provider.php
@@ -7,5 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Provider extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'user_id', 'description', 'experience', 'hourly_rate', 'available_from', 'available_to'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function services()
+    {
+        return $this->belongsToMany(Service::class);
+    }
+
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    public function reviews()
+    {
+        return $this->hasMany(Review::class);
+    }
+
+    public function advertisements()
+    {
+        return $this->hasMany(Advertisement::class);
+    }
 }
