929d3cefb924630630b20afcb9849880827cb557
FadwaElouah <fadwaelouah@gmail.com>
2025-04-07 00:51:48 +0100

    did route middleware

--- Home-servcie/routes/api.php
+++ Home-servcie/routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AdminController;
+use  App\Http\Controllers\ServiceController;
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\AdvertisementController;
+use App\Http\Controllers\ReviewController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +28,25 @@ Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register']);
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
+});
+Route::middleware('auth:api')->group(function () {
+
+    Route::resource('services', ServiceController::class);
+
+
+    Route::resource('bookings', BookingController::class);
+    Route::patch('bookings/{booking}/status', [BookingController::class, 'updateStatus']);
+
+
+    Route::resource('advertisements', AdvertisementController::class);
+
+
+    Route::resource('reviews', ReviewController::class);
+
+
+    Route::middleware('admin')->prefix('admin')->group(function () {
+        Route::get('users', [AdminController::class, 'getAllUsers']);
+        Route::get('dashboard', [AdminController::class, 'getDashboardData']);
+        Route::patch('users/{user}/status', [AdminController::class, 'toggleUserStatus']);
+    });
 });
