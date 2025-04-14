a673ea152013457c70658efe8b8f157f23794fd5
FadwaElouah <fadwaelouah@gmail.com>
2025-04-14 14:46:10 +0100

    add functions

--- Home-servcie/app/Http/Kernel.php
+++ Home-servcie/app/Http/Kernel.php
@@ -67,5 +67,6 @@ class Kernel extends HttpKernel
         'admin' => \App\Http\Middleware\CheckRole::class.':admin',
         'client' => \App\Http\Middleware\CheckRole::class.':client',
         'provider' => \App\Http\Middleware\CheckRole::class.':provider',
+       
     ];
 }
