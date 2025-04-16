c9681412f0163d4d8b2ad49462ada015c788c06d
FadwaElouah <fadwaelouah@gmail.com>
2025-04-16 10:29:55 +0100

    add services

--- Home-servcie/database/migrations/2025_04_06_232840_create_services_table.php
+++ Home-servcie/database/migrations/2025_04_06_232840_create_services_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('services', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price', 8, 2);
+            $table->foreignId('provider_id')->constrained('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
