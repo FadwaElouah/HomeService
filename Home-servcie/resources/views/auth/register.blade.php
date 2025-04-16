09c19215aea6d88b97bd06014df86d9ff4a27c5e
FadwaElouah <fadwaelouah@gmail.com>
2025-04-16 16:57:57 +0100

    add Header

--- Home-servcie/resources/views/auth/register.blade.php
+++ Home-servcie/resources/views/auth/register.blade.php
@@ -1,3 +1,17 @@
 @extends('layouts.app')
 @section('content')
 <div class="flex flex-col min-h-screen">
+  <!-- Header -->
+  <header class="p-4">
+    <div class="container mx-auto">
+        <div class="flex items-center">
+            <div class="flex items-center">
+                <img src="{{ asset('images/home-icon.svg') }}" alt="HomeService" class="w-6 h-6 mr-1">
+                <span class="font-bold">
+                    <span class="text-[#fd7600]">Home</span>
+                    <span>Service</span>
+                </span>
+            </div>
+        </div>
+    </div>
+</header>
