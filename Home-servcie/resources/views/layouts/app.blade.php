92adad914e2d678ebd9c2c6485dc2599317b47e8
FadwaElouah <fadwaelouah@gmail.com>
2025-04-16 17:18:45 +0100

    corriger les erreurs

--- Home-servcie/resources/views/layouts/app.blade.php
+++ Home-servcie/resources/views/layouts/app.blade.php
@@ -12,13 +12,30 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
 
-    <!-- Styles -->
-    @vite('resources/css/app.css')
+    <!-- Tailwind CSS via CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        primary: "#fd7600",
+                        secondary: "#007ab9",
+                        facebook: "#3b5998",
+                        twitter: "#55acee",
+                        linkedin: "#007ab9",
+                        lightgray: "#f1f2f2",
+                        darkgray: "#6a6a6a",
+                    }
+                }
+            }
+        }
+    </script>
 </head>
 <body class="font-sans antialiased">
     @yield('content')
 
     <!-- Scripts -->
-    @vite('resources/js/app.js')
+    <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
 </html>
