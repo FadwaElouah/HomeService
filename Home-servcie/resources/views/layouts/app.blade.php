2db01ae136d53dea2c5950a76d2957b11f86f2d9
FadwaElouah <fadwaelouah@gmail.com>
2025-04-16 16:54:49 +0100

    add body

--- Home-servcie/resources/views/layouts/app.blade.php
+++ Home-servcie/resources/views/layouts/app.blade.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -16,3 +15,10 @@
     <!-- Styles -->
     @vite('resources/css/app.css')
 </head>
+<body class="font-sans antialiased">
+    @yield('content')
+
+    <!-- Scripts -->
+    @vite('resources/js/app.js')
+</body>
+</html>
