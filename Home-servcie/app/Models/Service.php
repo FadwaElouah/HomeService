a591c9314b02c6d712316502082aba752d719297
FadwaElouah <fadwaelouah@gmail.com>
2025-04-26 10:33:10 +0100

    add model service

--- Home-servcie/app/Models/Service.php
+++ Home-servcie/app/Models/Service.php
@@ -5,7 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class Service extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'title', 'price', 'phone', 'description', 'image_path', 'status', 'admin_comment'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+    public function scopePending($query)
+    {
+        return $query->where('status', 'pending');
+    }
+
+    public function scopeApproved($query)
+    {
+        return $query->where('status', 'approved');
+    }
 }
